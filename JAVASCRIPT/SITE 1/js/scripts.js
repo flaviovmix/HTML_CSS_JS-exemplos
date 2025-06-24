8ffd8664ffba8b6fcbc7efec5244752b59f7d2aa
Flavio <flaviovmix@gmail.com>
2025-06-24 18:52:28 -0300

    ABOUT US

--- JAVASCRIPT/SITE 1/js/scripts.js
+++ JAVASCRIPT/SITE 1/js/scripts.js
@@ -8,12 +8,7 @@ document.getElementById("sliding-header-menu-close-button").onclick = function()
   document.getElementById("sliding-header-menu-outer").style.right = "-320px";
 }
 
-
-
-
-
 // About us Tab
-
 var aboutUs = {
   "Missão": "Fazer com que cada cliente seja reconhecido como autoridade em seu segmento de atuação. Agregar valor ao negócio, potencializar o crescimento das operações e promover e estreitar o relacionamento do cliente com o seu público alvo, por meio da geração de conteúdo de relevância.",
   "Visão": "Ser reconhecida pelos clientes e pelo mercado como uma empresa parceira, inovadora e criativa, que oferece sempre os melhores produtos e soluções em Comunicação Empresarial Integrada.",
@@ -21,7 +16,28 @@ var aboutUs = {
 };
 
 var unseletected_color = "#646872";
-var seletected_color = "#2A2D34";
+var selected_color = "#2A2D34";
+
+var about_tags = document.getElementsByClassName("single-tab");
+for(var a = 0; a < about_tags.length; a++) {
+
+  about_tags[a].onclick = function() {
+
+    for (var b = 0; b < about_tags.length; b++) {
+      about_tags[b].style['background-color'] = unseletected_color;
+      about_tags[b].style['font-weight'] = "200";      
+    }
+
+    this.style['background-color'] = selected_color;
+    this.style['font-weight'] = "bold";
+
+    var selecionado = this.innerHTML;
+
+    document.getElementById("box-text").innerHTML = aboutUs[selecionado];
+  }
+
+}
+
 
 
 
