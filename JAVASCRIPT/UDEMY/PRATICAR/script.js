5cca2dfa5a1d1571f64070f7722cc1e355f4f853
Flavio <flaviovmix@gmail.com>
2025-06-26 08:18:03 -0300

    COMECANDO A UDEMY

---
+++ JAVASCRIPT/UDEMY/PRATICAR/script.js
@@ -0,0 +1,29 @@
+var cursos = [
+
+    {
+        'titulo': 'Aprenda programação em Python 3 com facilidade do zero',
+        'avaliacoes': 680,
+        'alunos': 2300,
+        'categorias': ['programacao', 'tecnologia']
+    },
+
+    {
+        'titulo': 'Aprenda PHP e faça sites dinâmicos',
+        'avaliacoes': 180,
+        'alunos': 350,
+        'categorias': ['desenvolvimento web', 'programacao']
+    },
+
+    {
+        'titulo': 'Excel do Zero ao Avançado',
+        'avaliacoes': 420,
+        'alunos': 1800,
+        'categorias': ['produtividade', 'gestão']
+    }
+    
+];
+
+cursos[2].categorias[1] = "administracao de empresas";
+
+var conteudo = document.getElementById("conteudo");
+conteudo.innerHTML = cursos[2].categorias[1];
