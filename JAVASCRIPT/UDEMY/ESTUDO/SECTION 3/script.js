6e39dfb5c5212b5f7f511681264f2c64187f647f
Flavio <flaviovmix@gmail.com>
2025-06-28 09:32:22 -0300

    aula

--- JAVASCRIPT/UDEMY/ESTUDO/SECTION 3/script.js
+++ JAVASCRIPT/UDEMY/ESTUDO/SECTION 3/script.js
@@ -1,10 +1,34 @@
-var carro = {
-    'Ano': 2018,
-    'Modelo': 'Fox',
-    'Cilindradas': '1.8',
-    'Combustível': 'Gasolina'
-}
+var funcionarios = [
+    {
+        'nome': 'Carlos Henrique da Silva',
+        'idade': 45,
+        'filhos': ['Mariana Alves da Silva', 'Fernanda Alves da Silva']
+        
+    },
 
-for(var prop in carro) {
-    console.log(prop + ': ' + carro[prop]);
+    {
+        'nome': 'Maria Helena Pereira',
+        'idade': 32,
+        'filhos': undefined,
+        
+    },
+
+    {
+        'nome': 'José Feliciano Maia',
+        'idade': 39,
+        'filhos': ['Felipe Ferreira Maia', 'Fábio Ferreira Maia', 'João Ferreira Maia']
+        
+    }
+];
+
+var lista_elemento = document.getElementById("filhos");
+for(i=0; i<funcionarios.length; i++) {
+
+    if(funcionarios[i].filhos) {    
+        var lista_filhos = funcionarios[i].filhos;
+
+        for(j=0; j<lista_filhos.length; j++) {
+            lista_elemento.innerHTML += '<p>' + lista_filhos[j] + '<br/>Filhos de ' + funcionarios[i].nome + '</p>';
+        }
+}
 }
